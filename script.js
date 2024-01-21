ea782b500ab6341a7d76077c0f0acba10a8e6b0a
wasim2002 <wansari4540@gmail.com>
2024-01-21 13:34:19 +0530

    major update in js for add tie score increment

--- script.js
+++ script.js
@@ -1,64 +1,67 @@
-const boxes = document.querySelectorAll(".box")
+const Allbox = document.querySelectorAll(".box")
 const PlayerXScore = document.querySelector(".pX .score")
-// const tieScore = document.querySelector(".tie .score")
+const tieScore = document.querySelector(".tie .score")
 const PlayerOScore = document.querySelector(".pO .score")
 const resetBtn = document.querySelector(".reset")
 const newgameBtn = document.querySelector(".newGame")
 
-const winPattern = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-let turn = "x"
-boxes.forEach((box) => {
+let turn = "X";
+let arr = Array(9).fill(null);
+
+Allbox.forEach(box => {
     box.addEventListener("click", function () {
-        if (turn == "x") {
-            box.innerHTML = "X"
-            turn = "o"
-        } else {
-            box.innerHTML = "O"
-            turn = "x"
+        const id = box.id;
+        if (arr[id] === null) {
+            arr[id] = turn;
+            box.innerHTML = turn;
+            checkWinner();
+            turn = turn === "X" ? "O" : "X";
         }
-        box.disabled = true
-        checkWinner()
     })
 })
+
 const checkWinner = () => {
-    winPattern.forEach((Pattern) => {
-        let val1 = boxes[Pattern[0]].innerHTML
-        let val2 = boxes[Pattern[1]].innerHTML
-        let val3 = boxes[Pattern[2]].innerHTML
-
-        if (val1 != "" && val2 != "" && val3 != "") {
-            if (val1 == val2 && val2 == val3) {
-                winner(val1)
-            }
-        }
-
-    })
-
+    let condition1 = arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2];
+    let condition2 = arr[3] !== null && arr[3] === arr[4] && arr[4] === arr[5];
+    let consition3 = arr[6] !== null && arr[6] === arr[7] && arr[7] === arr[8];
+    let consition4 = arr[0] !== null && arr[0] === arr[3] && arr[3] === arr[6];
+    let consition5 = arr[1] !== null && arr[1] === arr[4] && arr[4] === arr[7];
+    let consition6 = arr[2] !== null && arr[2] === arr[5] && arr[5] === arr[8];
+    let consition7 = arr[0] !== null && arr[0] === arr[4] && arr[4] === arr[8];
+    let consition8 = arr[2] !== null && arr[2] === arr[4] && arr[4] === arr[6];
+    if (condition1 || condition2 || consition3 || consition4 || consition5 || consition6 || consition7 || consition8) {
+        arr.fill("")
+        showWinner(turn)
+    }
+    if (!arr.some((e) => e == null) && !arr.some((e) => e == "")) {
+        tieScore.innerHTML++
+    }
 }
-const winner = (winner) => {
-    boxes.forEach(box => {
-        box.disabled = true
-    })
-    if (winner == "X") {
+const showWinner = (player) => {
+    if (player == "X") {
         PlayerXScore.innerHTML++
-    } else {
+    }
+    if (player == "O") {
         PlayerOScore.innerHTML++
     }
 }
+
 const reset = () => {
-    boxes.forEach(box => {
+    arr.fill(null)
+    Allbox.forEach(box => {
         box.innerHTML = ""
-        box.disabled = false
     })
 }
-const newGame=()=>{
-    boxes.forEach(box => {
+const newGame = () => {
+    arr.fill(null)
+    turn = "O"
+    Allbox.forEach(box => {
         box.innerHTML = ""
-        box.disabled = false
     })
     PlayerXScore.innerHTML="0"
     PlayerOScore.innerHTML="0"
-    turn = "x"
+    tieScore.innerHTML="0"
 }
-newgameBtn.addEventListener("click",newGame)
+
+newgameBtn.addEventListener("click", newGame)
 resetBtn.addEventListener("click", reset)
